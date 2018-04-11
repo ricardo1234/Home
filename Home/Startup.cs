e1ccdfb3e9aee6f4339021eee23cccbeafe6d272
ricardo1234 <ricardo.ribeiro.21@hotmail.com>
2018-04-11 16:22:03 +0100

    Add project files.

---
+++ Home/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Home.Startup))]
+namespace Home
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
